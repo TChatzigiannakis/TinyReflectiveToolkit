9dd1a62eed78b10bd75ac5681deac502473de731
TChatzigiannakis <frozencompass@gmail.com>
2014-10-31 13:33:31 +0200

    Added IsDelegate().

---
+++ TinyReflectiveToolkit/IsDelegate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinyReflectiveToolkit
+{
+    public static partial class TypeExtensions
+    {
+        /// <summary>
+        /// Checks whether the given type is a delegate.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsDelegate(this Type type)
+        {
+            return typeof (Delegate).IsAssignableFrom(type);
+        }
+    }
+}
