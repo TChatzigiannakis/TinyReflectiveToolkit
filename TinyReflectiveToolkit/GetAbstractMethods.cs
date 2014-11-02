d9ad6f40db8903605240327286575b333739c598
TChatzigiannakis <frozencompass@gmail.com>
2014-11-02 13:49:00 +0200

    Moved some methods to their own files.

---
+++ TinyReflectiveToolkit/GetAbstractMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TinyReflectiveToolkit
+{
+    public static partial class TypeExtensions
+    {
+        /// <summary>
+        /// Returns all abstract methods of a given type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IEnumerable<MethodInfo> GetAbstractMethods(this Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+
+            return type.GetMethods().Where(x => x.IsAbstract);
+        }
+    }
+}
