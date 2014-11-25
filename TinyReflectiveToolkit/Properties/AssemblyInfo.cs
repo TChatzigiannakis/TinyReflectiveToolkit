900bcaf636421c06f8523d87a0bd46ed0419ee9d
TChatzigiannakis <frozencompass@gmail.com>
2014-11-25 19:02:28 +0200

    Updated to 0.5 and changed description.

--- TinyReflectiveToolkit/Properties/AssemblyInfo.cs
+++ TinyReflectiveToolkit/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("TinyReflectiveToolkit")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("Structural contracts, eager assembly loading, easier reflection.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Theodoros Chatzigiannakis")]
 [assembly: AssemblyProduct ("")]
@@ -16,7 +16,7 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.4.*")]
+[assembly: AssemblyVersion ("0.5.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
