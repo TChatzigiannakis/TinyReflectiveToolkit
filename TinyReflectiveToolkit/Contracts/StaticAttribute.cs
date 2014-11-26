cd4303c0df25dc7c06bebf44d4c63f2c3f11c953
TChatzigiannakis <frozencompass@gmail.com>
2014-11-26 13:08:05 +0200

    Added [Static].

---
+++ TinyReflectiveToolkit/Contracts/StaticAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinyReflectiveToolkit.Contracts
+{
+    /// <summary>
+    /// Declares that the type of the provided object must have a static method that matches the declared signature.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class StaticAttribute : Attribute
+    {
+    }
+}
