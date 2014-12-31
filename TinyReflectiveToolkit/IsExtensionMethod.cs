68861c55966fb158d6100cd5c15a24165ff8d8c9
TChatzigiannakis <frozencompass@gmail.com>
2014-12-31 16:48:35 +0200

    Added IsExtensionMethod().

---
+++ TinyReflectiveToolkit/IsExtensionMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using EnumerableExtensions;
+using System.Runtime.CompilerServices;
+
+namespace TinyReflectiveToolkit
+{
+    public static partial class TypeExtensions
+    {
+        /// <summary>
+        /// Checks whether a method can be called as an extension method.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        public static bool IsExtensionMethod(this MethodInfo method)
+        {
+            return method.ToUnarySequence().WithAttribute<ExtensionAttribute>().Any();
+        }
+
+    }
+}
