794114b534333ddac78796cd6da87a5d840b8887
TChatzigiannakis <frozencompass@gmail.com>
2015-01-02 12:55:05 +0200

    Updated license notice (#4).

--- TinyReflectiveToolkit/GetExtensionMethods.cs
+++ TinyReflectiveToolkit/GetExtensionMethods.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿/*
+ * TinyReflectiveToolkit
+ * Copyright (C) 2014-2015  Theodoros Chatzigiannakis
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
 
 namespace TinyReflectiveToolkit
 {
     public static partial class TypeExtensions
     {
-        //public static IEnumerable<T>
     }
 }
 
