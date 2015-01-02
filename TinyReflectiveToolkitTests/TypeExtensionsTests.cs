a39053387a9a5a6cf6a0bf38422df234666e0567
TChatzigiannakis <frozencompass@gmail.com>
2015-01-02 12:48:52 +0200

    Added update license notice.

--- TinyReflectiveToolkitTests/TypeExtensionsTests.cs
+++ TinyReflectiveToolkitTests/TypeExtensionsTests.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * TinyReflectiveToolkit
- * Copyright (C) 2014  Theodoros Chatzigiannakis
+ * Copyright (C) 2014-2015  Theodoros Chatzigiannakis
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
